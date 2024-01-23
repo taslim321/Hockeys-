70c9c92f87b2f48c89337447a6d6dbabaa6feaa8
Taslim Amin <121754467+taslim321@users.noreply.github.com>
2024-01-23 11:01:13 +0600

    card

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        backgroundImage: {
+          'hero-pattern': "url('/img/2.png')",
+          'footer-texture': "url('/img/footer-texture.png')",
+        }
+      },
     },
     plugins: [],
   }
